b0e032dd5f48793a7645ebcfe274525e393955f3
hc421003 <nkhshc421003@g.neec.ac.jp>
2025-01-20 16:00:31 +0900

    7-2-4

---
+++ 7-2/7-2-4.mjs
@@ -0,0 +1,8 @@
+import {kakeru} from "./7-2-4_kakeru_export.mjs"
+import {waru} from "./7-2-4_waru_export.mjs"
+
+let result = kakeru(10,2)
+console.log(result)
+
+result = waru(10,2)
+console.log(result)
