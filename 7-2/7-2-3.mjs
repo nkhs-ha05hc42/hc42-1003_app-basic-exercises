f20cae1fb8bc740791b144b098438151459b58c2
hc421003 <nkhshc421003@g.neec.ac.jp>
2025-01-20 16:00:20 +0900

    7-2-3

---
+++ 7-2/7-2-3.mjs
@@ -0,0 +1,7 @@
+import {A,B} from "./7-2-3_export.mjs"
+
+let result = A(10,2)
+console.log(result)
+
+result = B(10,2)
+console.log(result)
---
+++ 7-2/7-2-3_export.mjs
@@ -0,0 +1,7 @@
+export const A = (a,b) => {
+    return a * b
+}
+
+export const B = (a,b) => {
+    return a / b
+}
