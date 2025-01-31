213ac2e1a615cd2001a885edcaa31f0f109457de
hc421003 <nkhshc421003@g.neec.ac.jp>
2025-02-01 00:55:28 +0900

    8-1

--- server/api/8-1/q8_1Controller.mjs
+++ server/api/8-1/q8_1Controller.mjs
@@ -1,4 +1,4 @@
-const getKadai8_1 = (req, res) => {
+const getKadai8_1_1 = (req, res) => {
     res.send(
         JSON.stringify({
             code: req.query.code,
@@ -7,6 +7,38 @@ const getKadai8_1 = (req, res) => {
     )
 }
 
+const postKadai8_1_2 = (req, res) => {
+    console.log(req.body)
+    res.send(
+        JSON.stringify({
+            status: "OK",
+            postcode: req.body.postcode,
+        }),
+    )
+}
+
+const putKadai8_1_3 = (req, res) => {
+    console.log(req.body)
+    res.send(
+        JSON.stringify({
+            status: "OK",
+            postcode: req.body.postcode,
+        }),
+    )
+}
+
+const deleteKadai8_1_4 = (req, res) => {
+    res.send(
+        JSON.stringify({
+            status: "OK",
+            code: req.query.code
+        })
+    )
+}
+
 export const q8_1Controller = {
-    getKadai8_1,
+    getKadai8_1_1,
+    postKadai8_1_2,
+    putKadai8_1_3,
+    deleteKadai8_1_4,
 }
