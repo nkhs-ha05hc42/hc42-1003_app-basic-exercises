b810c3ad3526559a5298f9d94697fd2baaf12cf2
hc421003 <nkhshc421003@g.neec.ac.jp>
2025-02-17 13:50:00 +0900

    8-3

---
+++ server/api/8-3/q8_3Controller.mjs
@@ -0,0 +1,58 @@
+const getKadai8_3_1 = (req, res) => {
+    const fruit = [
+        {
+            en: "apple",
+            ja: "りんご"
+        },
+        {
+            en: "lemon",
+            ja: "レモン"
+        },
+        {
+            en: "grape",
+            ja: "ぶどう"
+        }
+        
+    ]
+    res.send(
+        fruit
+    )
+}
+
+const getKadai8_3_2 = (req, res) => {
+    const fruit = [
+        {
+            en: "apple",
+            ja: "りんご"
+        },
+        {
+            en: "lemon",
+            ja: "レモン"
+        },
+        {
+            en: "grape",
+            ja: "ぶどう"
+        }
+        
+    ]
+
+    for (let i = 1; i <= fruit.length; i++){
+        if (fruit[i].en == req.params.en) {
+            res.send( fruit[i] )
+        }
+        else{
+            res.send(
+                JSON.stringify({
+                    status: "error",
+                    en: req.params.en,
+                    cause: "not found!"
+                })
+            )
+        }
+    }
+}
+
+export const q8_3Controller = {
+    getKadai8_3_1,
+    getKadai8_3_2,
+}
