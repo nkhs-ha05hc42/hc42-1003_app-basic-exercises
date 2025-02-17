5027a173d6d8198a71494fe05985666fc06c0881
hc421003 <nkhshc421003@g.neec.ac.jp>
2025-02-17 13:49:51 +0900

    server

--- server/routers.mjs
+++ server/routers.mjs
@@ -26,6 +26,7 @@ routers.put("/api/8-2", q8_2Controller.putKadai8_2_3)
 routers.delete("/api/8-2", q8_2Controller.deleteKadai8_2_4)
 
 routers.get("/api/8-3", q8_3Controller.getKadai8_3_1)
+routers.get("/api/8-3/:en", q8_3Controller.getKadai8_3_2)
 
 routers.post("/api/sample2", sample2Controller.postSample2)
 routers.get("/api/sample2/:id", sample2Controller.getOneSample2)
